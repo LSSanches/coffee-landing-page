504e8ec9c05d7e236bfb56ebd95ff7baf4691a6c
laura_sanches <laurassanches@gmail.com>
2022-11-21 15:52:52 -0500

    basic HTML landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import LandingPage from "./LandingPage.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header"></header>
+      <main>
+        {" "}
+        <LandingPage />
+      </main>
+      <div className="read-more">
+        <strong>Read more at </strong>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.ncausa.org/About-Coffee/History-of-Coffee"
+          target="_blank"
+          rel="noreferrer"
+        >
+          NCA - National Coffee Association
+        </a>
+      </div>
+      <footer>
+        Created by{" "}
+        <a
+          href="https://www.linkedin.com/in/laurassanches"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LSanches
         </a>
-      </header>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/LSSanches/dictionary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://dictionarybylaura.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
---
+++ src/LandingPage.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import "./LandingPage.css";
+
+export default function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <h1>Welcome Coffee Lovers!</h1>
+      <img
+        src="https://media.istockphoto.com/id/825154518/vector/red-coffee-mug-with-steam-in-flat-design-style-vector-illustration.jpg?s=612x612&w=0&k=20&c=yIHt6EiPNteF_ZlWGCAVMfVyvHUzI8bk2uKCAq0d2vc="
+        alt="coffee mug"
+      ></img>
+      <h2>
+        No one knows exactly how or when coffee was discovered, though there are
+        many legends about its origin.
+      </h2>
+      <h3>The History of Coffee</h3>
+      <h4>An Ethiopian Legend</h4>
+      <p>
+        Coffee grown worldwide can trace its heritage back centuries to the
+        ancient coffee forests on the Ethiopian plateau. There, legend says the
+        goat herder Kaldi first discovered the potential of these beloved beans.
+        The story goes that that Kaldi discovered coffee after he noticed that
+        after eating the berries from a certain tree, his goats became so
+        energetic that they did not want to sleep at night. Kaldi reported his
+        findings to the abbot of the local monastery, who made a drink with the
+        berries and found that it kept him alert through the long hours of
+        evening prayer. The abbot shared his discovery with the other monks at
+        the monastery, and knowledge of the energizing berries began to spread.
+        As word moved east and coffee reached the Arabian peninsula, it began a
+        journey which would bring these beans across the globe.
+      </p>
+      <h4>The Arabian Peninsula</h4>
+      <p>
+        Coffee cultivation and trade began on the Arabian Peninsula. By the 15th
+        century, coffee was being grown in the Yemeni district of Arabia and by
+        the 16th century it was known in Persia, Egypt, Syria, and Turkey.
+        Coffee was not only enjoyed in homes, but also in the many public coffee
+        houses — called qahveh khaneh — which began to appear in cities across
+        the Near East. The popularity of the coffee houses was unequaled and
+        people frequented them for all kinds of social activity. Not only did
+        the patrons drink coffee and engage in conversation, but they also
+        listened to music, watched performers, played chess and kept current on
+        the news. Coffee houses quickly became such an important center for the
+        exchange of information that they were often referred to as “Schools of
+        the Wise.” With thousands of pilgrims visiting the holy city of Mecca
+        each year from all over the world, knowledge of this “wine of Araby”
+        began to spread.
+      </p>
+    </div>
+  );
+}
